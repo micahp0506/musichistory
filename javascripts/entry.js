2ffae4787d2a11b34afa399f469a30cc4a3ac24b
micahp0506 <micahp0506@gmail.com>
2015-11-12 09:24:40 -0600

    Transfering to another branch

--- javascripts/entry.js
+++ javascripts/entry.js
@@ -7,11 +7,16 @@ requirejs.config({
     "jquery": "../lib/bower_components/jquery/dist/jquery.min",
     "bootstrap": "../lib/bower_components/bootstrap/dist/js/bootstrap.min",
     "hbs": "../lib/bower_components/require-handlebars-plugin/hbs",
-    "lodash": "../lib/bower_components/lodash/lodash.min"
+    "lodash": "../lib/bower_components/lodash/lodash.min",
+    "firebase": "../lib/bower_components/firebase/firebase"
   },
+
   shim: {
   	"bootstrap": ["jquery"]
-  }
+  },
+    "firebase": {
+    exports: "Firebase"
+    } 
 });
 
 
